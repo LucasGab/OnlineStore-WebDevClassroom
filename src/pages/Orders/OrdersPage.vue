5c6606453037250fe22c77eaab1774c75dcfaba2
Lucas Gabriel de Araujo Silva <lucasarasil1@gmail.com>
2021-07-03 02:13:54 -0300

    Total functionality (#4)
    * Added cart functionality
    
    * Added partial wishes and orders page
    
    * Added partial wishes and orders page

---
+++ src/pages/Orders/OrdersPage.vue
@@ -0,0 +1,57 @@
+<template>
+  <main>
+    <section class="cover-main-section">
+      <h1>Orders</h1>
+    </section>
+    <section class="orders-container">
+      <table>
+        <thead>
+          <tr>
+            <th scope="col">Orders</th>
+            <th scope="col">Status</th>
+            <th scope="col">Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr class="order-item">
+            <td style="width: 60%">ID: 3123559120</td>
+            <td style="width: 10%">APROVED</td>
+            <td style="width: 10%">$100.00</td>
+          </tr>
+          <tr class="order-item">
+            <td style="width: 60%">ID: 3123559120</td>
+            <td style="width: 10%">WAITING</td>
+            <td style="width: 10%">$250.00</td>
+          </tr>
+          <tr v-for="book in cart" :key="book.name">
+            <td style="width: 60%">
+              <div class="book-item">
+                <img
+                  :src="book.imgUrl"
+                  :alt="book.name"
+                  height="220"
+                  width="150"
+                />
+                <h4>{{ book.name }}</h4>
+              </div>
+            </td>
+            <td style="width: 10%">
+              <button class="erase-button" type="button">
+                <i class="fas fa-trash"></i>
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </section>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "OrdersPage",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped src="./orders.css"></style>
